a7beed52a647e6534c14c730aee078b515757ac5
Shreets <shreeti.019@gmail.com>
2021-05-14 11:19:38 +0545

    disable next button until content loads

--- src/pages/PatientChecklist.js
+++ src/pages/PatientChecklist.js
@@ -7,6 +7,7 @@ const PatientChecklist = ({ state, setState, setPage, page }) => {
   const [symptoms, setSymptoms] = useState([]);
   const [symptomsError, setSymptomsError] = useState(false);
   const [isSymptomChecked, setIsSymptomChecked] = useState(false);
+  const [isSymptomLoad, setIsSymptomLoad] = useState(false);
 
   useEffect(() => {
     getSymptoms();
@@ -57,7 +58,13 @@ const PatientChecklist = ({ state, setState, setPage, page }) => {
   const getSymptoms = async () => {
     const response = await patientService.getPatientSymtoms();
     setSymptoms(response.symptomsTemplate);
+    if (symptoms) {
+      setIsSymptomLoad(true);
+    } else {
+      setIsSymptomLoad(false);
+    }
   };
+
   return (
     <div className="form-content-wrapper">
       <div>Are you currently having any of the following symptoms?</div>
@@ -89,7 +96,11 @@ const PatientChecklist = ({ state, setState, setPage, page }) => {
           At least one field must be selected
         </span>
       ) : null}
-      <button className="submit-button submit-btn" onClick={onNext}>
+      <button
+        className="submit-button submit-btn"
+        onClick={onNext}
+        disabled={isSymptomLoad}
+      >
         NEXT
       </button>
     </div>
