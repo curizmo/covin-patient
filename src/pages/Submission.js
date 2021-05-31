f92f1327e859cb5ceb085ced5f159f56831e96a3
Shreets <shreeti.019@gmail.com>
2021-05-31 12:10:36 +0545

    conditionally displaying thank you message

--- src/pages/Submission.js
+++ src/pages/Submission.js
@@ -11,47 +11,40 @@ const Submission = ({ messageStatus }) => {
 
   return (
     <>
-      {messageStatus === MESSAGE_STATUS.sent ? (
-        <>
-          <div className="form-content-wrapper success-page">
-            <img
-              className="thank-you-img"
-              src={success_image}
-              alt="success-image"
-            />
-            <div className="text-greeting">
-              <div>
-                <span className="bold-text">Thank you</span> for updating your
-                health information.
-              </div>
-              <br />
-              <div>You will hear from us shortly.</div>
-              <br />
-              <br />
-              <div>You may close the window.</div>
+      <>
+        <div
+          className={`form-content-wrapper success-page ${
+            messageStatus === MESSAGE_STATUS.processed && "expired-page"
+          }`}
+        >
+          <img
+            className="thank-you-img"
+            src={success_image}
+            alt="success-image"
+          />
+          <div className="text-greeting">
+            <div>
+              <span className="bold-text">Thank you</span>{" "}
+              {messageStatus === MESSAGE_STATUS.processed
+                ? ""
+                : "for updating your health information."}
+            </div>
+            <br />
+            <div>
+              {messageStatus === MESSAGE_STATUS.processed
+                ? "You have already submitted the form."
+                : "You will hear from us shortly."}
+            </div>
+            <br />
+            <br />
+            <div>
+              {messageStatus === MESSAGE_STATUS.processed
+                ? ""
+                : "You may close the window."}
             </div>
           </div>
-        </>
-      ) : (
-        <>
-          <div className="form-content-wrapper expired-page">
-            <img
-              className="thank-you-img"
-              src={success_image}
-              alt="success-image"
-            />
-            <div className="text-greeting">
-              <div>
-                <span className="bold-text">Thank you</span>
-              </div>
-              <br />
-              <div>You have already submitted the form.</div>
-              <br />
-              <br />
-            </div>
-          </div>
-        </>
-      )}
+        </div>
+      </>
     </>
   );
 };
