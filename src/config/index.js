239198728f6ded0ee3eda8d09092670fcc90e66b
Shreets <shreeti.019@gmail.com>
2021-06-28 14:55:30 +0545

    show jitsi modal on web meeting status in InProgress

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,12 @@
 const config = {
   apiURL: process.env.REACT_APP_API_URL,
+  jitsi: {
+    appId: process.env.REACT_APP_JITSI_APP_ID,
+  },
+  pusher: {
+    key: process.env.REACT_APP_CORS_PUSHER_KEY,
+    cluster: process.env.REACT_APP_CORS_PUSHER_CLUSTER,
+  },
 };
 
 export default config;
--- src/pages/WebMeetWaitingRoom.js
+++ src/pages/WebMeetWaitingRoom.js
@@ -1,16 +1,146 @@
-import React, { useEffect, useState, useMemo } from "react";
+/*global JitsiMeetExternalAPI */
+import React, {
+  useEffect,
+  useState,
+  useMemo,
+  useRef,
+  useCallback,
+} from "react";
 import "./home.css";
 import * as patientService from "../services/patient";
 import { getDateString, getTimeString, getToday } from "../utils";
 import { BOOKING_STATUS } from "../constants/constants";
+import config from "../config/index";
+import Pusher from "pusher-js";
 
 const WebMeetWaitingRoom = ({ patientDetails }) => {
   const [appointment, setAppointment] = useState({});
+  const jitsiContainer = useRef(null);
+  const [jitsiApi, setJitsiApi] = useState(null);
+  const [startWebMeeting, setStartWebMeeting] = useState(false);
+  const [practitionerDetails, setPractitionerDetails] = useState({});
+
   const today = getToday();
   useEffect(() => {
     window.scrollTo(0, 0);
     getAppointmentDetail();
   }, []);
+
+  const { key, cluster } = config.pusher;
+  const pusher = new Pusher(key, {
+    cluster,
+    encrypted: true,
+  });
+
+  const handleAppointmentProgress = useCallback(
+    (data) => {
+      console.log(appointment);
+
+      setAppointment({ ...appointment, eventStatusDesc: "InProgress" });
+
+      if (data.practitioner) {
+        setPractitionerDetails(data.practitioner);
+      }
+
+      if (data) {
+        setStartWebMeeting(true);
+      }
+    },
+    [appointment]
+  );
+
+  useEffect(() => {
+    if (appointment.organizationEventBookingId && !startWebMeeting) {
+      const appointmentStatus = pusher.subscribe("appointmentStatus");
+
+      appointmentStatus.bind("InProgress", handleAppointmentProgress);
+    }
+  }, [appointment, startWebMeeting]);
+
+  const getJWT = async () => {
+    const response = await patientService.fetchJWT(patientDetails.patientId);
+
+    return response.jwt;
+  };
+
+  const startConference = useCallback(async () => {
+    console.log('i am running')
+    if (
+      patientDetails.patientId &&
+      appointment &&
+      appointment.eventStatusDesc === BOOKING_STATUS.inProgress &&
+      patientDetails.patientId === appointment.patientId
+    ) {
+      const jwt = await getJWT();
+      console.log("hi");
+
+      try {
+        const domain = "8x8.vc";
+        const options = {
+          roomName: `${config.jitsi.appId}/curizmo-${appointment.organizationEventBookingId}`,
+          width: "100%",
+          height: "100%",
+          parentNode: jitsiContainer.current,
+          jwt,
+          interfaceConfigOverwrite: {
+            filmStripOnly: false,
+            SHOW_JITSI_WATERMARK: false,
+            HIDE_INVITE_MORE_HEADER: true,
+            DISABLE_FOCUS_INDICATOR: true,
+          },
+          userInfo: {
+            displayName: `${patientDetails.givenName} ${patientDetails.familyName}`,
+          },
+          configOverwrite: {
+            startWithVideoMuted: false,
+            disableSimulcast: false,
+            disableDeepLinking: true,
+            disableFocusIndicator: true,
+            disableInviteFunctions: true,
+            disableThirdPartyRequests: true,
+            prejoinPageEnabled: true,
+            // Note: This is a hack for hiding the room name of the call.
+            // The feature is not built in Jitsi and they've said it's low priority.
+            // Might need to send a PR there.
+            subject: " ",
+          },
+        };
+
+        const api = new JitsiMeetExternalAPI(domain, options);
+
+        setJitsiApi(api);
+      } catch (err) {
+        // @toDo Handle error
+      }
+    }
+  }, [jitsiContainer, appointment?.organizationEventBookingId]);
+
+  useEffect(() => {
+    if (
+      patientDetails.patientId &&
+      appointment &&
+      appointment.eventStatusDesc === BOOKING_STATUS.inProgress &&
+      patientDetails.patientId === appointment.patientId
+    ) {
+      // Load the jitsi script
+      const script = document.createElement("script");
+      script.src = "https://8x8.vc/external_api.js";
+      document.body.appendChild(script);
+      // start the call only after the above script finishes loading
+      script.onload = () => {
+        // verify the JitsiMeetExternalAPI constructor is added to the global..
+        if (window.JitsiMeetExternalAPI) {
+          startConference();
+        } else {
+          // TODO: Remove alert and add better error handler.
+          alert("Jitsi Meet API script not loaded");
+        }
+      };
+      return () => {
+        document.body.removeChild(script);
+      };
+    }
+  }, [startConference]);
 
   const getAppointmentDetail = async () => {
     try {
@@ -54,6 +184,15 @@ const WebMeetWaitingRoom = ({ patientDetails }) => {
                 Appointment Time : {appointmentStartDate.time}
               </p>
               <p>Please wait for the physician to bring you into exam room.</p>
+            </div>
+          </div>
+        </div>
+      ) : appointmentStartDate.date === today &&
+        appointment.eventStatusDesc === BOOKING_STATUS.inProgress ? (
+        <div className={`web-call-page`}>
+          <div className="waiting-room-message">
+            <div>
+              <div ref={jitsiContainer} className="jitsi-embed"></div>
             </div>
           </div>
         </div>
